9c8e599d8b2bcaac834cc565e49bee7cd7ef03a2
Hollywood42 <hollywood42@hollywood42-cards.localdomain>
2017-04-05 15:50:56 +0000

    Updated todo list

--- index.php
+++ index.php
@@ -4,20 +4,19 @@
 
 <html>
 	<body>
-		<h1>Work in progress</h1>
-		<p>More to come soon</p>
-
-		<h3>Todos:</h3>
+		<h1>Todos:</h1>
 		<ul>
 			<li>Nav bar styling</li>
 			<ul>
+				<li>Get it positioned correctly</li>
 				<li>Figure out what rules are being used</li>
 				<li>Narrow it down to one file and modify/customize from there</li>
 			</ul>
 			<li>Continue adding cards to database</li>
 			<li>Set up galleries page</li>
-			<li>Set up full info page</li>
+			<li>Clean up full info page</li>
 			<ul>
+				<li>Better display for empty fields</li>
 				<li>Search/sort</li>
 				<li>Likely applicable to galleries search/sort as well</li>
 			</ul>
@@ -25,7 +24,14 @@
 			<li>API for deleting records from database</li>
 			<li>Admin section for adding cards, deleting cards, etc</li>
 			<li>Fix password stuff</li>
+			<li>Fill in wantlist pages</li>
+			<ul>
+				<li>Text of actual wants</li>
+				<li>Decide on best layout</li>
+				<li>Style</li>
+			</ul>
 		</ul>
 		<h3>Test section</h3>
+			<p><i>To be used for misc. testing when needed</i></p>
 	</body>
 </html>
