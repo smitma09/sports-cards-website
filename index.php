5b7d30ccd5aacad783e65c16aaf103c89ca22ca4
Hollywood42 <smitma09@luther.edu>
2017-04-06 15:37:49 +0000

    Minor CSS changes

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 				<li>Get it positioned correctly</li>
 				<li>Figure out what rules are being used</li>
 				<li>Narrow it down to one file and modify/customize from there</li>
-				<li>Investigate why it doesn't appear styled in certain directories</li>
 			</ul>
 			<li>Continue adding cards to database</li>
 			<li>Set up galleries page</li>
