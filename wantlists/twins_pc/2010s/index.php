9b238d55ff0e27ecdb3161672e4a8ea18336f82a
Hollywood42 <smitma09@luther.edu>
2017-04-07 20:20:57 +0000

    Wantlist update

--- wantlists/twins_pc/2010s/index.php
+++ wantlists/twins_pc/2010s/index.php
@@ -144,17 +144,393 @@
 
 	<br>
 
+	<p><u>2016 Bowman Platinum</u><br>
+	<b>Base:</b> 22, 37, 71, 81, 84, 93<br>
+	<b>Green (#/99):</b> 22, 37, 71, 81, 84, 93<br>
+	<b>Ice:</b> 22, 37, 71, 81, 84, 93<br>
+	<b>Orange (#/25):</b> 22, 37, 71, 81, 84, 93<br>
+	<b>Purple (#/250):</b> 22, 37, 71, 81, 84, 93<br>
+	<b>Top Prospects:</b> ABW, NG, TJ, WJ<br>
+	<b>Top Prospects Green (#/99):</b> ABW, NG, TJ, WJ<br>
+	<b>Top Prospects Ice:</b> ABW, NG, TJ, WJ<br>
+	<b>Top Prospects Orange (#/25):</b> ABW, NG, TJ, WJ<br>
+	<b>Top Prospects Purple (#/250):</b> ABW, NG, TJ, WJ<br>
+	<b>Next Generation:</b> 17<br>
+	<b>Next Generation Green (#/99):</b> 17<br>
+	<b>Next Generation Orange (#/25:</b> 17<br>
+	<b>Next Generation Purple (#/250):</b> 17<br>
+	<b>Next Generation Prospects:</b> 17<br>
+	<b>Next Generation Prospects Green (#/99):</b> 17<br>
+	<b>Next Generation Prospects Orange (#/25:</b> 17<br>
+	<b>Next Generation Prospects Purple (#/250):</b> 17</p>
+	
+	<br>
 
-
-
-
-
-
-
-
-
-
-
+	<p><u>2016 Diamond Kings</u><br>
+	<i>Need to check back and see if variations have parallels or not</i><br>
+	<b>Base:</b> 46a (swinging, bat visible), 46b (swinging, bat not visible VAR), 79, 143a (white jersey), 143b (blue jersey VAR), 167<br>
+	<b>Artist's Proof (#/99):</b> 46, 79, 143, 167<br>
+	<b>Artist's Proof Silver (#/25):</b> 46, 79, 143, 167<br>
+	<b>Framed:</b> 46, 79, 143, 167<br>
+	<b>Framed Blue (#/99):</b> 46, 79, 143, 167<br>
+	<b>Framed Red (#/99):</b> 46, 79, 143, 167<br>
+	<b>DK Minis:</b> 46, 79, 143, 167<br>
+	<b>DK Originals:</b> 16<br>
+	<b>DK Originals Sapphire (#/25):</b> 16<br>
+	<b>Expressionists:</b> 18<br>
+	<b>Expressionists Sapphire (#/25):</b> 18</p>
+	
+	<br>
+	
+	<p><u>2016 Donruss</u><br>
+	<i>Completed- Rated Rookies Die Cut Blue (#/999)</i><br>
+	<b>Base:</b> 17, 35, 113, 115, 178a, 178b ("Hall of Fame 2001" SP)<br>
+	<b>Black (#/199):</b> 17, 35, 113, 115, 178<br>
+	<b>Career Stat Line (#ing varies):</b> 17, 35, 113, 115, 178<br>
+	<b>Gold Press Proof (#/99):</b> 17, 35, 113, 115, 178<br>
+	<b>Pink:</b> 17, 35, 113, 115, 178<br>
+	<b>Season Stat Line (#ing varies):</b> 17, 35, 113, 115, 178<br>
+	<b>Test Proof Black (#/25):</b> 17, 35, 113, 115, 178<br>
+	<b>Test Proof Cyan (#/49):</b> 17, 35, 113, 115, 178<br>
+	<b>Rated Rookies Die Cut Black (#/25):</b> 1<br>
+	<b>Rated Rookies Die Cut Green (#/99):</b> 1<br>
+	<b>Rated Rookies Die Cut Red (#/299):</b> 1<br>
+	<b>1982:</b> 42<br>
+	<b>1982 Black Border (#/99):</b> 42<br>
+	<b>1982 Gold Press Proof (#/49):</b> 42<br>
+	<b>1982 Holoflow (#/199):</b> 42<br>
+	<b>1982 Holometric (#/299):</b> 42<br>
+	<b>1982 Pink:</b> 42<br>
+	<b>1982 Test Proof Black (#/25):</b> 42<br>
+	<b>1982 Test Proof Cyan (#/49):</b> 42<br>
+	<b>Elite Series (#/999):</b> 20<br>
+	<b>Power Alley:</b> 5<br>
+	<b>Power Alley Blue (#/199):</b> 5<br>
+	<b>Power Alley Disco (#/299):</b> 5<br>
+	<b>Power Alley Red (#/99):</b> 5<br>
+	<b>Studio:</b> 2, 4<br>
+	<b>Studio Gold Press Proof (#/99):</b> 2, 4<br>
+	<b>Studio Red Border (#/199):</b> 2, 4<br>
+	<b>Studio Test Proof Black (#/25):</b> 2, 4<br>
+	<b>Studio Test Proof Cyan (#/49):</b> 2, 4<br>
+	<b>The Prospects:</b> 11<br>
+	<b>The Prospects Black Border (#/199):</b> 11<br>
+	<b>The Prospects Career Stat Line (#ed):</b> 11<br>
+	<b>The Prospects Gold Press Proof (#/99):</b> 11<br>
+	<b>The Prospects Season Stat Line (#ed):</b> 11<br>
+	<b>The Prospects Test Proof Black (#/25):</b> 11<br>
+	<b>The Rookies:</b> 11<br>
+	<b>The Rookies Black Border (#/199):</b> 5<br>
+	<b>The Rookies Career Stat Line (#ed):</b> 5<br>
+	<b>The Rookies Gold Press Proof (#/99):</b> 5<br>
+	<b>The Rookies Season Stat Line (#ed):</b> 5<br>
+	<b>The Rookies Test Proof Black (#/25):</b> 5</p>
+	
+	<br>
+	
+	<p><u>2016 Donruss Optic</u><br>
+	<b>Base:</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Aqua (#/299):</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Black (#/25):</b> 17, 35, 46, 54, 60, 159<br>
+	<b>Blue (#/149):</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Carolina Blue (#/50):</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Holo:</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Orange (#/199):</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Pink:</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Purple:</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Red (#/99):</b> 17, 35, 46, 54, 60, 93, 159<br>
+	<b>Back to the Future:</b> 15<br>
+	<b>Back to the Future Blue (#/149):</b> 15<br>
+	<b>Back to the Future Red (#/99):</b> 15<br>
+	<b>Power Alley:</b> 5<br>
+	<b>Power Alley Blue (#/149):</b> 5<br>
+	<b>Power Alley Red (#/99):</b> 5<br>
+	<b>The Rookies:</b> 5<br>
+	<b>The Rookies Blue (#/149):</b> 5<br>
+	<b>The Rookies Red (#/99):</b> 5</p>
+	
+	<br>
+	
+	<p><u>2016 Panini Diamond Kings</u><br>
+	<b>Base:</b> 46a (swing), 46b (after swing, bat not visible SP) 79, 143a (white jersey), 143b (blue jersey SP), 167<br>
+	<b>Artist's Proof (#/99):</b> 46a (swing), 46b (after swing, bat not visible SP) 79, 143a (white jersey), 143b (blue jersey SP), 167<br>
+	<b>Artist's Proof Silver (#/25):</b> 46a (swing), 46b (after swing, bat not visible SP) 79, 143a (white jersey), 143b (blue jersey SP), 167<br>
+	<b>Framed:</b> 46a (swing), 46b (after swing, bat not visible SP) 79, 143a (white jersey), 143b (blue jersey SP), 167<br>
+	<b>Framed Blue (#/99):</b> 46a (swing), 46b (after swing, bat not visible SP) 79, 143a (white jersey), 143b (blue jersey SP), 167<br>
+	<b>Framed Red (#/99):</b> 46a (swing), 46b (after swing, bat not visible SP) 79, 143a (white jersey), 143b (blue jersey SP), 167<br>
+	<b>DK Minis:</b> 46, 79, 143, 167<br>
+	<b>DK Minis Black (#/25):</b> 46, 79, 143, 167<br>
+	<b>DK Originals:</b> 16<br>
+	<b>DK Originals Sapphire (#/25):</b> 16<br>
+	<b>Expressionists:</b> 18<br>
+	<b>Expressionists Sapphire (#/25):</b> 18</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Series 1</u><br>
+	<i>Completed- MLB Debut</i><br>
+	<b>Base:</b> 78a (swinging), 78b (running out of dugout SSP), 129, 286b (white/camo jersey SP), 286c (bat over head SSP)<br>
+	<b>Black (#/65):</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Gold (#/2,016):</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Negative:</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Pink (#/50):</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Rainbow Foil:</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Toys-R-Us Purple:</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Vintage Stock (#/99):</b> 76, 78, 129, 161, 181, 208, 235, 262, 286, 308<br>
+	<b>Berger's Best (BB):</b> 17, 51<br>
+	<b>Perspectives (P):</b> 6<br>
+	<b>Pressed Into Service (PIS):</b> 4</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Series 2</u>
+	<i>Completed- Berger's Best</i><br>
+	<b>Base:</b> 360, 528, 534, 546, 560, 624<br>
+	<b>Black (#/65):</b> 360, 475, 528, 534, 546, 560, 561, 576, 591, 624, 645<br>
+	<b>Gold (#/2,016):</b> 360, 475, 528, 534, 546, 560, 561, 576, 591, 624, 645<br>
+	<b>Negative:</b> 360, 475, 528, 534, 546, 560, 561, 576, 591, 624, 645<br>
+	<b>Pink (#/50):</b> 360, 475, 528, 534, 546, 560, 561, 576, 591, 624, 645<br>
+	<b>Rainbow Foil:</b> 360, 475, 528, 534, 546, 560, 561, 576, 591, 624, 645<br>
+	<b>Vintage Stock (#/99):</b> 360, 475, 528, 534, 546, 560, 561, 576, 591, 624, 645<br>
+	<b>Berger's Best (BB):</b> 1971 Bert Blyleven<br>
+	<b>Bunt Player Cards (#/25):</b> Miguel Sano<br>
+	<b>MLB Debut:</b> 15<br>
+	<b>Laser:</b> MSA</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Allen & Ginter</u><br>
+	<i>Completed- Baseball Legends</i><br>
+	<b>Base:</b> 66, 78, 154, 234<br>
+	<b>Mini:</b> 24, 74, 154, 305, 348, 389<br>
+	<b>Mini A&G Back:</b> 24, 66, 74, 78, 104, 154, 201, 234, 348<br>
+	<b>Mini Black:</b> 24, 66, 74, 78, 104, 154, 201, 234, 305, 348<br>
+	<b>Mini Brooklyn Back (#/25):</b> 24, 66, 74, 78, 104, 154, 201, 234, 305, 348<br>
+	<b>Mini No Number (PR 50):</b> 24, 66, 74, 78, 104, 154, 201, 234, 305, 348<br>
+	<b>Mini Wood (#/1)- HAVE:</b> 305<br>
+	<b>Mascots in the Wild:</b> 5<br>
+	<b>Mini Skippers:</b> 19<br>
+	<b>Numbers Game:</b> 93<br>
+	<b>Rip Cards (may be ripped or unripped):</b> 39, 84, 94</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Allen & Ginter X</u><br>
+	<i>Double check this, should be parallels</i><br>
+	<b>Base:</b> 24, 66, 74, 78, 104, 154, 201, 234, 305, 348</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Archives</u><br>
+	<b>Base:</b> 24, 58, 71, 85, 105, 127, 149, 196, 237, 238, 242, 261<br>
+	<b>Blue Border (#/199):</b> 24, 58, 71, 85, 105, 127, 149, 196, 237, 238, 242, 261<br>
+	<b>Operation Desert Shield (only cards 201-300?):</b> 24, 58, 71, 85, 105, 127, 149, 196, 237, 238, 242, 261<br>
+	<b>Red Border (#/50):</b> 24, 58, 71, 85, 105, 127, 149, 196, 237, 238, 242, 261<br>
+	<b>1969 Topps Super:</b> MS<br>
+	<b>1969 Topps Super Red (#/50):</b> MS<br>
+	<b>1985 Topps #1 Draft Pick:</b> JM<br>
+	<b>1985 Topps #1 Draft Pick Red (#/50):</b> JM</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Bunt</u><br>
+	<b>Base:</b> 27, 75, 76, 96, 154, 167<br>
+	<b>Platinum (#/99):</b> 27, 75, 76, 96, 154, 167<br>
+	<b>Topaz (#/50):</b> 27, 75, 76, 96, 154, 167<br>
+	<b>Future of the Franchise:</b> 2, 6<br>
+	<b>Moon Shots:</b> 23<br>
+	<b>Program:</b> 25</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Chrome</u><br>
+	<i>Completed- Base, Refractors</i><br>
+	<b>Black Refractors:</b> 13, 83, 99, 104<br>
+	<b>Blue Refractors (#/150):</b> 13, 82, 83, 99, 104, 138<br>
+	<b>Blue Wave Refractors (#/75):</b> 13, 82, 83, 99, 104, 138<br>
+	<b>Green Refractors (#/99):</b> 13, 82, 83, 99, 104, 138, 183<br>
+	<b>Gold Refractors (#/50):</b> 13, 82, 99, 104, 138, 183<br>
+	<b>Orange Refractors (#/25):</b> 82, 83, 99, 104, 138, 183<br>
+	<b>Pink Refractors:</b> 82, 99, 104<br>
+	<b>Prism Refractors:</b> 82, 104, 183<br>
+	<b>Purple Refractors (#/275):</b> 13, 82, 83, 99, 104, 138, 183<br>
+	<b>Sepia Refractors:</b> 99, 104, 138<br>
+	<b>Future Stars:</b> 11<br>
+	<b>Future Stars Green:</b> 11<br>
+	<b>Future Stars Orange (#/25):</b> 11<br>
+	<b>Perspectives:</b> 10, 17<br>
+	<b>Youth Impact:</b> 2, 6, 19<br>
+	<b>Youth Impact Green:</b> 2, 6, 19<br>
+	<b>Youth Impact Orange (#/25):</b> 2, 6</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Finest</u><br>
+	<b>Base:</b> 31, 41, 69, 94<br>
+	<b>Refractor:</b> 31, 41, 69, 94<br>
+	<b>Prism Refractor:</b> 31, 41, 69, 94<br>
+	<b>Blue Refractor (#/250):</b> 31, 41, 69, 94<br>
+	<b>Gold Refractor (#/50):</b> 31, 41, 69, 94<br>
+	<b>Green Refractor (#/99):</b> 31, 41, 69, 94<br>
+	<b>Orange Refractor (#/25):</b> 31, 41, 69, 94<br>
+	<b>Purple Refractor (#/250):</b> 31, 41, 69, 94<br>
+	<b>Finest Firsts:</b> MS<br>
+	<b>Finest Firsts Orange Refractor (#/25):</b> MS<br>
+	<b>Franchise Finest:</b> BD<br>
+	<b>Franchise Finest Orange Refractor (#/25):</b> BD</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Gypsy Queen</u><br>
+	<b>Base:</b> 16a (batting), 16b (fielding SP) 36, 121, 141, 207, 221, 263, 328, 340, 347<br>
+	<b>Mini:</b> 16a (batting), 16b (baserunning SP), 36, 121, 141, 207, 263, 328, 340, 347<br>
+	<b>Mini Purple (#/250):</b> 16, 36, 121, 141, 207, 263, 328, 340, 347<br>
+	<b>Mini Gold (#/50):</b> 16, 36, 121, 141, 207, 263, 328, 340, 347<br>
+	<b>Mini Red (#/5)- HAVE:</b> 221<br>
+	<b>Blue Framed:</b> 16, 36<br>
+	<b>Green Framed (#/99):</b> 16, 36<br>
+	<b>Purple Framed (#/250):</b> 16, 36<br>
+	<b>Power Alley:</b> 22</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Heritage</u><br>
+	<i>Completed- Baseball Flashbacks</i><br>
+	<b>Base:</b> 50a, 50b (color swap SP), 57, 112, 175, 182, 190, 300, 334, 374b (error SP- no line under career stats), 380, 427<br>
+	<b>Flip Stock:</b> 50, 57, 66, 112, 175, 182, 190, 194, 269, 299, 300, 334, 350, 374, 380, 427<br>
+	<b>Gum Stain Backs:</b> 50, 57, 66, 112, 175, 182, 190, 194, 269, 299, 300, 334, 350, 374, 380, 427<br>
+	<b>Red Backs:</b> 50, 57, 66, 112, 175, 182, 190, 194, 269, 299, 300, 334, 350, 374, 380, 427<br>
+	<b>1967 Topps Punch-Outs (67POB):</b> CC, LS<br>
+	<b>Ad Panels:</b> Coming soon!<br>
+	<b>Chrome (#/999):</b> 50, 190, 427<br>
+	<b>Chrome Refractors (/567):</b> 50, 190, 427<br>
+	<b>Chrome Black Refractors (#/67):</b> 50, 190, 427<br>
+	<b>Chrome Purple Refractors:</b> 50, 190, 427<br>
+	<b>Mini (#/100):</b> 50, 66, 374, 427<br>
+	<b>New Age Performers (NAP):</b> MSA<br>
+	<b>Then and Now (TAN):</b> KV</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Heritage High Number</u><br>
+	<b>Base:</b> 708a, 708b (color swap SP), 709, 717<br>
+	<b>Black:</b> 612, 613, 627, 645, 646, 670, 708, 717<br>
+	<b>Flip Stock:</b> 612, 613, 627, 645, 646, 670, 708, 709, 717<br>
+	<b>Gum Stain:</b> 612, 613, 627, 645, 646, 670, 708, 709, 717<br>
+	<b>Red Back:</b> 612, 613, 627, 645, 646, 670, 708, 709, 717<br>
+	<b>1967 Punchboards:</b> CY, FR, RC<br>
+	<b>Ad Panels:</b> Coming soon!<br>
+	<b>Chrome (#/999):</b> 708, 709, 717<br>
+	<b>Chrome Refractor (#/567):</b> 708, 709, 717<br>
+	<b>Chrome Black (#/67):</b> 708, 709, 717<br>
+	<b>Chrome Purple Refractor:</b> 708, 709, 717<br>
+	<b>Rookie Performers:</b> JB, MS</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Marketside Pizza</u>
+	<b>Base:</b> 14, 42</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Mini</u><br>
+	<i>Coming soon</i></p>
+	
+	<br>
+	
+	<p><u>2016 Topps MLB Sticker Collection</u><br>
+	<b>Base:</b> 110 Mauer, 111 Dozier, 112 Plouffe, 113 Rosario, 114 Buxton, 115 Perkins, 116 Suzuki, 117 Hughes, 118 Sano, 145 Twins</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Museum Collection</u><br>
+	<b>Base:</b> 38, 64<br>
+	<b>Blue (#/99):</b> 38, 64<br>
+	<b>Copper:</b> 38, 64<br>
+	<b>Green (#/199):</b> 38, 64</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Now</u><br>
+	<i>Due to the day by day release of these cards, I'll list out which ones I need individually. I need the following cards-</i><br>
+	<b>108</b> Miguel Sano extends HR streak (PR 721)<br>
+	<b>133</b> Brian Dozier walkoff HR (PR 293)<br>
+	<b>182</b> Albert Pujols passes Harmon Killebrew (PR 413)<br>
+	<b>248</b> Rod Carew & Tony Gwynn batting titles (PR 693)<br>
+	<b>304</b> Max Kepler 3 HR game (PR 620)<br>
+	<b>330</b> Miguel Sano hits roof, 2 HR (PR 588)<br>
+	<b>427</b> Brian Dozier 3 HR game (PR 306)<br>
+	<b>441</b> Joe Mauer 12th inning walkoff (PR 281)<br>
+	<b>449</b> Brian Dozier, Harmon Killebrew Twins 40-HR seasons (PR 483)<br>
+	<b>491</b> Brian Dozier AL 2B HR record (PR 324)<br>
+	<b>493</b> Byron Buxton robs home run (PR 323)</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Opening Day</u><br>
+	<b>Base:</b> 97<br>
+	<b>Blue Foil:</b> 43, 52, 97, 124, 140<br>
+	<b>Purple Foil:</b> 43, 52, 97, 124, 140<br>
+	<b>Alternate Reality (AR):</b> 10<br>
+	<b>Heavy Hitters (HH):</b> 5<br>
+	<b>Mascots (M):</b> 10<br>
+	<b>Opening Day Stars (ODS):</b> 23<br>
+	<b>Superstar Celebrations (SC):</b> 11</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Spring Fever</u><br>
+	<b>Base:</b> 35, 40</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Stadium Club</u><br>
+	<b>Base:</b> 81a (Sano with bat), 81b (Sano laughing VAR), 118, 127, 184, 191, 234, 252, 282, 285, 296<br>
+	<b>Beam Team:</b> 12<br>
+	<b>Beam Team Gold:</b> 12<br>
+	<b>Triumvirates Luminous:</b> 6a (Sano)<br>
+	<b>Triumvirates Luminescent:</b> 6a (Sano)<br>
+	<b>Triumvirates Illuminator:</b> 6a (Sano)</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Tribute</u><br>
+	<b>Base:</b> 40, 69<br>
+	<b>Green (#/99):</b> 40, 69<br>
+	<b>Purple (#/50):</b> 40, 69<br>
+	<b>2016 Rookies:</b> 3<br>
+	<b>2016 Rookies Purple (#/50):</b> 3</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Triple Threads</u><br>
+	<b>Base:</b> 60<br>
+	<b>Amber (#/150):</b> 60<br>
+	<b>Amethyst (#/340):</b> 60<br>
+	<b>Emerald (#/250):</b> 60<br>
+	<b>Gold (#/99):</b> 60<br>
+	<b>Onyx (#/50):</b> 60<br>
+	<b>Sapphire (#/25):</b> 60</p>
+	
+	<br>
+	
+	<p><u>2016 Topps Update</u><br>
+	<i>Completed- 500 Home Run Futures</i><br>
+	<i>Posted before set release, double check this for additions/errors later</i><br>
+	<b>Base:</b> 17, 31, 65, 168, 176, 228<br>
+	<b>Black (#/64):</b> 17, 31, 65, 168, 171, 176, 228<br>
+	<b>Gold (#/2,016):</b> 17, 31, 65, 168, 171, 176, 228<br>
+	<b>Pink (#/50):</b> 17, 31, 65, 168, 171, 176, 228<br>
+	<b>Rainbow Foil:</b> 17, 31, 65, 168, 171, 176, 228<br>
+	<b>Toys-R-Us Purple:</b> 17, 31, 65, 168, 171, 176, 228<br>
+	<b>3,000 Hit Club:</b> 6<br>
+	<b>500 Home Run Club:</b> HK<br>
+	<b>Fire:</b> 12<br>
+	<b>Retail Exclusive Rookies Target:</b> 3, 12, 18<br>
+	<b>Retail Exclusives Rookies Walmart:</b> 3, 12, 18<br>
+	<b>Team Franklin:</b> 18</p>
+	
+	<hr>
 
 <body>
 <html>
