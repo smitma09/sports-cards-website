8eeca317fa9ed6ff0d99c252c8ce039d7d0649c2
Hollywood42 <smitma09@luther.edu>
2017-04-07 16:41:57 +0000

    Wantlist update

--- wantlists/twins_pc/2010s/index.php
+++ wantlists/twins_pc/2010s/index.php
@@ -6,14 +6,13 @@
 <body>
 	<h1> Twins PC wants- 2010s</h1>
 
+	<p>This page will be used to list all of my wants from sets in the 2010s (2010-2019). This is currently a work in progress as a slowly move my set wants over from my old website to this one. Please note that autographs and relics will not be included on these wants- I still would be interested in them if you have any, but I'm not listing them here in order to save space. Cards numbered to less than 25 will also not be listed. I would like those as well, so please let me know if you have any available.</p>
+
 	<img alt="Card test" src="https://sportscardalbum.com/c/i2z53s58.JPG">
 	
-	<p><u>Set name</u><br>
-	<b>Base:</b> 1, 2, 3, 4, 5<br>
-	<b>Insert 1:</b> 1, 2, 3, 4, 5<br>
-	<b>Insert 2:</b> 1, 2, 3, 4, 5</p>
+	<h1>2017</h1>
 
-	<h3>2017</h3>
+	<br>
 
 	<p><u>2017 Topps Series 1</u><br>
 	<b>Base:</b> 8a (white jersey), 8b (red jersey SSP), 27, 126a (white jersey), 126b (red jersey SSP), 227a (catching ball), 227b (on deck swing SSP), 304, 305, 336<br>
@@ -33,8 +32,9 @@
 	<b>Jackie Robinson Day:</b> 10, 18<br>
 	<b>Jackie Robinson Day Red (#/25):</b> 10, 18</p>
 
+	<br>
 
-<p><u>2016 Topps Heritage</u>
+	<p><u>2017 Topps Heritage</u><br>
 	<b>Base:</b> 32, 80, 95, 128, 142, 179, 206, 220, 243, 304, 313, 337, 436<br>
 	<b>Blue (PR 50):</b> 32, 80, 95, 128, 141, 142, 179, 206, 220, 243, 271, 304, 313, 337, 436, 465<br>
 	<b>Yellow Back (PR 25):</b> 32, 80, 95, 128, 141, 142, 179, 206, 220, 243, 271, 304, 313, 337, 436, 465<br>
@@ -47,6 +47,51 @@
 	<b>Chrome Blue Refractor (#/68):</b> 436, 465<br>
 	<b>Chrome Purple Refractor:</b> 436, 465<br>
 	<b>Mini (#/100):</b> 220, 243, 436, 465</p>
+
+	<hr>
+
+	<h1>2016</h1>
+
+	<br>
+
+	<p><u>2016 Bowman</u><br>
+	<i>Completed- Base, Chrome Prospects (BCP), Prospects (BP), Sophomore Standouts</i><br>
+	<b>Blue (#/150):</b> 52, 54, 140, 147<br>
+	<b>Gold (#/50):</b> 52, 53, 54, 140, 147<br>
+	<b>Green (#/99):</b> 52, 53, 54, 140, 147<br>
+	<b>Orange (#/25):</b> 52, 53, 54, 140, 147<br>
+	<b>Purple (#/250):</b> 52, 53, 54, 140, 147<br>
+	<b>Silver:</b> 52, 53, 54, 140, 147<br>
+	<b>Yellow:</b> 52, 53, 54, 140, 147<br>
+	<b>Chrome Prospects Refractors (BCP):</b> 32, 61, 97, 135<br>
+	<b>Chrome Prospects Blue Refrators (BCP, #/150):</b> 32, 61, 97, 135<br>
+	<b>Chrome Prospects Gold Refractors (BCP, #/50):</b> 32, 61, 97, 135<br>
+	<b>Chrome Prospects Green Refractors (BCP, #/99):</b> 32, 61, 97, 135<br>
+	<b>Chrome Prospects Green Shimmer Refractors (BCP, #/99):</b> 32, 97, 135<br>
+	<b>Chrome Prospects Orange Refractors (BCP, #/25):</b> 32, 61, 97, 135<br>
+	<b>Chrome Prospects Orange Shimmer Refractors (BCP, #/25):</b> 32, 61, 97, 135<br>
+	<b>Chrome Prospects Purple Refractors (BCP, #/250):</b> 32, 61, 97, 135<br>
+	<b>Prospects Blue (BP, #/150):</b> 32, 61, 97, 135<br>
+	<b>Prospects Gold (BP, #/50):</b> 32, 61, 97, 135<br>
+	<b>Prospects Green (BP, #/99):</b> 32, 61, 97, 135<br>
+	<b>Prospects Orange (BP, #/25):</b> 32, 61, 97, 135<br>
+	<b>Prospects Purple (BP, #/250):</b> 32, 61, 97, 135<br>
+	<b>Prospects Silver (BP):</b> 32, 61, 97, 135<br>
+	<b>Prospects Yellow (BP):</b> 32, 61, 97, 135<br>
+	<b>Bowman Scout's Top 100:</b> 2, 12, 78, 82<br>
+	<b>Bowman Scout's Top 100 Gold (#/50):</b> 2, 12, 29, 78, 82<br>
+	<b>Bowman Scout's Top 100 Green (#/99):</b> 2, 12, 29, 78, 82<br>
+	<b>Bowman Scout's Top 100 Orange (#/25):</b> 2, 12, 29, 78, 82<br>
+	<b>International Ink:</b> WJ<br>
+	<b>International Ink Blue (#/150):</b> WJ<br>
+	<b>International Ink Green (#/99):</b> WJ<br>
+	<b>International Ink Orange (#/25):</b> WJ<br>
+	<b>Sophomore Standouts Blue (#/150):</b> 2<br>
+	<b>Sophomore Standouts Green (#/99):</b> 2<br>
+	<b>Sophomore Standouts Orange (#/25):</b> 2<br>
+	<b>Turn Two: BJ</p>
+
+	<br>
 
 <body>
 <html>
