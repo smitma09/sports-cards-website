67aad907c8d83b8f725cccfc78d2949149513465
Hollywood42 <smitma09@luther.edu>
2017-04-08 16:22:54 +0000

    Minor CSS edits, updated wantlist organization

--- wantlists/twins_pc/index.php
+++ wantlists/twins_pc/index.php
@@ -5,5 +5,16 @@
 <html>
 <body>
 	<h1>Twins PC wants</h1>
+	<p>This section of my site will hold lists of the Twins cards I need for my PC. My wants are broken down by year as follows:</p>
+	<ul>
+		<li><a href="./2015-2019.php">2015-2019</a></li>
+		<li>2010-2014</li>
+		<li>2005-2009</li>
+		<li>2000-2004</li>
+		<li>1990-1999</li>
+		<li>1980-1989</li>
+		<li>1970-1979</li>
+		<li>1961-1969</li>
+	</ul>
 </body>
 </html>
