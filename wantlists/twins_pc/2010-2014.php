f656171f2168014321b71eb468fc3a7d85b53e70
Hollywood42 <smitma09@luther.edu>
2017-04-13 15:25:45 +0000

    Wantlist update

--- wantlists/twins_pc/2010-2014.php
+++ wantlists/twins_pc/2010-2014.php
@@ -13,11 +13,9 @@
 
 	<h1>2014</h1>
 
-	<i>Still need to enclose each set listing in p tags</i>
-
 	<br>
 
-	<div class=title>2014 Bowman</div><br>
+	<p><div class=title>2014 Bowman</div><br>
 	<i>Completed- Base, Chrome Prospects</i><br>
 	<div class=ssTitle>Black (#/25):</div> 6, 173, 189<br>
 	<div class=ssTitle>Blue (#/500):</div> 173, 189<br>
@@ -59,11 +57,11 @@
 	<div class=ssTitle>Prospects Silver Ice:</div> 8, 32<br>
 	<div class=ssTitle>'89 Bowman is Back:</div> BB<br>
 	<div class=ssTitle>Chrome Top 100 Prospects:</div> 1, 3, 90, 94<br>
-	<div class=ssTitle>Chrome Top 100 Prospects Die Cut Refractors (#/99):</div> 1, 3, 54, 82, 90, 94<br>
+	<div class=ssTitle>Chrome Top 100 Prospects Die Cut Refractors (#/99):</div> 1, 3, 54, 82, 90, 94</p>
 
 	<br>
 
-	<div class=title>2014 Bowman Chrome</div><br>
+	<p><div class=title>2014 Bowman Chrome</div><br>
 	<i>Completed- Fire</i><br>
 	<div class=ssTitle>Base:</div> 13, 134, 208<br>
 	<div class=ssTitle>Refractors (#ed/500):</div> 13, 134, 203<br>
@@ -96,19 +94,19 @@
 	<div class=ssTitle>Prospects Series 2 Pink Wave Refractors (BCP):</div> 84, 96, 98<br>
 	<div class=ssTitle>Prospects Series 2 Purple Refractors (BCP, #ed/150):</div> 84, 96, 98<br>
 	<div class=ssTitle>Prospects Series 2 Red Wave Refractors (BCP, #ed/25):</div> 84, 96, 98<br>
-	<div class=ssTitle>Prospects Series 2 Silver Wave Refractors (BCP, #ed/25):</div> 96, 98<br>
+	<div class=ssTitle>Prospects Series 2 Silver Wave Refractors (BCP, #ed/25):</div> 96, 98</p>
 
 	<br>
 
-	<div class=title>2014 Bowman Chrome Mini</div><br>
+	<p><div class=title>2014 Bowman Chrome Mini</div><br>
 	<div class=ssTitle>Base:</div> 74, 98, 125, 128, 133, 152, 154, 233. 261, 265, 312<br>
 	<div class=ssTitle>Refractors:</div> 74, 98, 125, 128, 133, 152, 154, 233. 261, 265, 312<br>
 	<div class=ssTitle>Black Shimmer Refractors:</div> 74, 98, 125, 128, 133, 152, 154, 233. 261, 265, 312<br>
-	<div class=ssTitle>Yellow Refractors (#/25):</div> 74, 98, 125, 128, 133, 152, 154, 233. 261, 265, 312<br>
+	<div class=ssTitle>Yellow Refractors (#/25):</div> 74, 98, 125, 128, 133, 152, 154, 233. 261, 265, 312</p>
 
 	<br>
 
-	<div class=title>2014 Bowman Draft ---</div><br>
+	<p><div class=title>2014 Bowman Draft ---</div><br>
 	<i>Completed- Future of the Franchise Mini</i><br>
 	<div class=ssTitle>Base (DP):</div> 44, 126<br>
 	<div class=ssTitle>Blue (DP, #/399):</div> 44, 93, 121, 126<br>
@@ -154,11 +152,11 @@
 	<div class=ssTitle>Bowman Initiation (BI, #/99):</div> NG<br>
 	<div class=ssTitle>Draft Night (DN):</div> NG<br>
 	<div class=ssTitle>Dual Draftees (DD):</div> SG<br>
-	<div class=ssTitle>Future of the Franchise Mini Chrome Black (FF):</div> MS<br>
+	<div class=ssTitle>Future of the Franchise Mini Chrome Black (FF):</div> MS</p>
 
 	<br>
 
-	<div class=title>2014 Bowman Platinum</div><br>
+	<p><div class=title>2014 Bowman Platinum</div><br>
 	<i>(This set listing looks wierd to me. Going to have to review it and make sure it is accurate when this set has been out for awhile longer)</i><br>
 	<i>Completed- Prospects, Cutting Edge Stars</i><br>
 	<div class=ssTitle>Chrome Prospects Refractors:</div> 5, 10, 11, 34, 36<br>
@@ -175,21 +173,21 @@
 	<div class=ssTitle>Toolsy Die Cuts 5-Tool Parallel:</div> BB<br>
 	<div class=ssTitle>Top Prospects:</div> BB<br>
 	<div class=ssTitle>Top Prospects Blue (#/49):</div> BB, MS<br>
-	<div class=ssTitle>Top Prospects Die Cuts (#/25):</div> BB, MS<br>
+	<div class=ssTitle>Top Prospects Die Cuts (#/25):</div> BB, MS</p>
 
 	<br>
 
-	<div class=title>2014 Donruss</div><br>
+	<p><div class=title>2014 Donruss</div><br>
 	<div class=ssTitle>Base:</div> 21, 60<br>
 	<div class=ssTitle>Press Proofs Silver (#/199):</div> 21, 60, 167<br>
 	<div class=ssTitle>Press Proofs Gold (#/99):</div> 21, 60, 167<br>
 	<div class=ssTitle>Stat Line Career (#/323 or 54):</div> 21, 60, 167<br>
 	<div class=ssTitle>Stat Line Season (#/324 or 36):</div> 21, 60, 167<br>
-	<div class=ssTitle>Team MVPs:</div> 7, 25<br>
+	<div class=ssTitle>Team MVPs:</div> 7, 25</p>
 
 	<br>
 
-	<div class=title>2014 Donruss Series 2</div><br>
+	<p><div class=title>2014 Donruss Series 2</div><br>
 	<i>Completed- Base, Diamond Kings, Elite</i><br>
 	<div class=ssTitle>Press Proofs Silver (#/199):</div> 315<br>
 	<div class=ssTitle>Press Proofs Gold (#/99):</div> 314, 315<br>
@@ -202,16 +200,16 @@
 	<div class=ssTitle>Elite Gold Status Die Cut (#/49):</div> 69<br>
 	<div class=ssTitle>Elite Red Status Die Cut (#/25):</div> 69<br>
 	<div class=ssTitle>Elite Inspirations Die Cut (#/93):</div> 69<br>
-	<div class=ssTitle>Elite Dominator (#/999):</div> 14<br>
+	<div class=ssTitle>Elite Dominator (#/999):</div> 14</p>
 
 	<br>
 
-	<div class=title>2014 Donruss The Rookies</div><br>
-	<div class=ssTitle>Base:</div> 69, 78<br>
+	<p><div class=title>2014 Donruss The Rookies</div><br>
+	<div class=ssTitle>Base:</div> 69, 78</p>
 
 	<br>
 
-	<div class=title>2014 Finest</div><br>
+	<p><div class=title>2014 Finest</div><br>
 	<i>Completed- '94 Finest</i><br>
 	<div class=ssTitle>Base:</div> 44, 60<br>
 	<div class=ssTitle>Refractors:</div> 44, 60<br>
@@ -222,16 +220,16 @@
 	<div class=ssTitle>Orange Refractors:</div> 44, 60<br>
 	<div class=ssTitle>Red Refracots (#/25):</div> 44, 60<br>
 	<div class=ssTitle>X-Fractors:</div> 44, 60<br>
-	<div class=ssTitle>'94 Finest Refractors (#/25):</div> JM<br>
+	<div class=ssTitle>'94 Finest Refractors (#/25):</div> JM</p>
 
 	<br>
 
-	<div class=title>2014 Immaculate Collection</div><br>
-	<div class=ssTitle>Base (#ed/99):</div> 9<br>
+	<p><div class=title>2014 Immaculate Collection</div><br>
+	<div class=ssTitle>Base (#ed/99):</div> 9</p>
 
 	<br>
 
-	<div class=title>2014 Panini Prizm</div><br>
+	<p><div class=title>2014 Panini Prizm</div><br>
 	<i>Completed- Base</i><br>
 	<div class=ssTitle>Prizms:</div> 86<br>
 	<div class=ssTitle>42 Prizms (#/42):</div> 86, 104<br>
@@ -249,11 +247,11 @@
 	<div class=ssTitle>Next Era Prizms:</div> 4, 5<br>
 	<div class=ssTitle>Next Era Prizms Blue Mojo (#/75):</div> 4, 5<br>
 	<div class=ssTitle>Next Era Prizms Purple (#/99):</div> 4, 5<br>
-	<div class=ssTitle>Next Era Prizms Red (#/25):</div> 4, 5<br>
+	<div class=ssTitle>Next Era Prizms Red (#/25):</div> 4, 5</p>
 
 	<br>
 
-	<div class=title>2014 Stadium Club</div><br>
+	<p><div class=title>2014 Stadium Club</div><br>
 	<div class=ssTitle>Base:</div> 160<br>
 	<div class=ssTitle>Electric Foil:</div> 35, 160<br>
 	<div class=ssTitle>Foilboard (#/25):</div> 35, 160<br>
@@ -266,11 +264,11 @@
 	<div class=ssTitle>Field Access Rainbow (#/99):</div> 17<br>
 	<div class=ssTitle>Triumvirates Luminous:</div> T8C (Mauer) <br>
 	<div class=ssTitle>Triumvirates Illuminator:</div> T8C (Mauer)<br>
-	<div class=ssTitle>Triumvirates Luminescent:</div> T8C (Mauer)<br>
+	<div class=ssTitle>Triumvirates Luminescent:</div> T8C (Mauer)</p>
 
 	<br>
 
-	<div class=title>2014 Topps</div><br>
+	<p><div class=title>2014 Topps</div><br>
 	<i>Completed- 50 Years of the Draft, Saber Stars</i><br>
 	<div class=ssTitle>Base:</div> 103, 125b (Sparkle variation), 125c (Sabermetric stats variation), 385<br>
 	<div class=ssTitle>Acetate/Clear (#/10)- HAVE:</div> 120<br>
@@ -287,11 +285,11 @@
 	<div class=ssTitle>Yellow:</div> 12, 88, 103, 120, 125, 162, 220, 221, 325, 385, 405, 441, 463, 488, 492, 518, 541, 568, 624<br>
 	<div class=ssTitle>1989 Die-Cut Minis:</div> 43<br>
 	<div class=ssTitle>Power Players:</div> 104, 171, 217<br>
-	<div class=ssTitle>Spring Fever:</div> 23<br>
+	<div class=ssTitle>Spring Fever:</div> 23</p>
 
 	<br>
 
-	<div class=title>2014 Topps Allen & Ginter</div><br>
+	<p><div class=title>2014 Topps Allen & Ginter</div><br>
 	<i>Completed- Base</i><br>
 	<div class=ssTitle>Mini:</div> 51, 199<br>
 	<div class=ssTitle>Mini A&G Back:</div> 51, 199, 258<br>
@@ -301,18 +299,18 @@
 	<div class=ssTitle>Mini Red (#/50):</div> 51, 199, 258<br>
 	<div class=ssTitle>Mini Metal:</div> 126<br>
 	<div class=ssTitle>Rip Cards (may be ripped or unripped):</div> 36, 72<br>
-	<div class=ssTitle>The Pastime's Pastime:</div> HK<br>
+	<div class=ssTitle>The Pastime's Pastime:</div> HK</p>
 
 	<br>
 
-	<div class=title>2014 Topps Archives</div><br>
+	<p><div class=title>2014 Topps Archives</div><br>
 	<i>Completed- Base</i><br>
 	<div class=ssTitle>Gold (#/199):</div> 43, 154<br>
-	<div class=ssTitle>Silver (#/99):</div> 43, 142, 154<br>
+	<div class=ssTitle>Silver (#/99):</div> 43, 142, 154</p>
 
 	<br>
 
-	<div class=title>2014 Topps Chipz</div><br>
+	<p><div class=title>2014 Topps Chipz</div><br>
 	<div class=ssTitle>Base:</div> 60<br>
 	<div class=ssTitle>Black:</div> 60<br>
 	<div class=ssTitle>Blue:</div> 60<br>
@@ -336,11 +334,11 @@
 	<div class=ssTitle>Magnets Red:</div> 60<br>
 	<div class=ssTitle>Magnets Silver:</div> 60<br>
 	<div class=ssTitle>Mascots:</div> 14<br>
-	<div class=ssTitle>Teams:</div> 17<br>
+	<div class=ssTitle>Teams:</div> 17</p>
 
 	<br>
 
-	<div class=title>2014 Topps Chrome</div><br>
+	<p><div class=title>2014 Topps Chrome</div><br>
 	<i>Completed- Base</i><br>
 	<div class=ssTitle>Refractors:</div> 24, 84, 144<br>
 	<div class=ssTitle>Black Refractors (#/100):</div> 24, 84, 144<br>
@@ -350,22 +348,22 @@
 	<div class=ssTitle>Purple Refractors:</div> 84, 144<br>
 	<div class=ssTitle>Red Refractors (#/25):</div> 24, 84, 144<br>
 	<div class=ssTitle>Sepia Refractors (#/75):</div> 24, 84, 144<br>
-	<div class=ssTitle>X-Fractors:</div> 84<br>
+	<div class=ssTitle>X-Fractors:</div> 84</p>
 
 	<br>
 
-	<div class=title>2014 Topps Gypsy Queen</div><br>
+	<p><div class=title>2014 Topps Gypsy Queen</div><br>
 	<div class=ssTitle>Base:</div> 157a (Mauer), 157b (Mauer reverse neg. SP blue jersey)<br>
 	<div class=ssTitle>Framed Blue (#/499):</div> 153, 157<br>
 	<div class=ssTitle>Framed White:</div> 153<br>
 	<div class=ssTitle>Mini:</div> 127, 153, 157a (Mauer throwback jersey), 187, 195<br>
 	<div class=ssTitle>Mini Black (#/199):</div> 127, 153, 157, 187, 195<br>
 	<div class=ssTitle>Mini Red (#/99):</div> 127, 153, 157, 187, 195<br>
-	<div class=ssTitle>Mini Sepia (#/50):</div> 127, 153, 157, 187, 195<br>
+	<div class=ssTitle>Mini Sepia (#/50):</div> 127, 153, 157, 187, 195</p>
 
 	<br>
 
-	<div class=title>2014 Topps Heritage</div><br>
+	<p><div class=title>2014 Topps Heritage</div><br>
 	<i>Completed- First Draft</i><br>
 	<div class=ssTitle>Base:</div> 62b ("Coreia" spelling error), 169, 464a (SP), 464b (Throwback SP), 464c (Logo SP)<br>
 	<div class=ssTitle>Black Back:</div> 1, 62, 88, 89, 90, 169, 180, 201, 237, 315, 340, 341, 419, 464<br>
@@ -375,16 +373,16 @@
 	<div class=ssTitle>Chrome Refractors (#/565):</div> 464<br>
 	<div class=ssTitle>Chrome Black Refractors (#/65):</div> 464<br>
 	<div class=ssTitle>Chrome Purple Refractors:</div> 464<br>
-	<div class=ssTitle>Embossed:</div> 14<br>
+	<div class=ssTitle>Embossed:</div> 14</p>
 
 	<br>
 
-	<div class=title>2014 Topps Heritage High Number</div><br>
-	<div class=ssTitle>Base (H):</div> 526, 566, 586, 587<br>
+	<p><div class=title>2014 Topps Heritage High Number</div><br>
+	<div class=ssTitle>Base (H):</div> 526, 566, 586, 587</p>
 
 	<br>
 
-	<div class=title>2014 Topps High Tek</div><br>
+	<p><div class=title>2014 Topps High Tek</div><br>
 	<i>You'll notice that I'm listing all parallels for this set, even though some are numbered to less than 25. This is because I think I am going to attempt to put together the rainbow for this set. So, if you have any of the cards below (all Mauer cards), please let me know! Because these parallels are hard for me to keep track of, black means I still need it, red means I have one already. This way I can keep track of what's what. Thanks much!</i><br>
 	<div class=ssTitle>Large Shattered:</div> JM<br>
 	<div class=ssTitle>Large Scribble:</div> JM<br>
@@ -404,66 +402,66 @@
 	<div class=ssTitle>Wave Press Proofs Cyan (#ed/1):</div> JM<br>
 	<div class=ssTitle>Wave Press Proofs Magenta (#ed/1):</div> JM<br>
 	<div class=ssTitle>Wave Press ProofsYellow (#ed/1):</div> JM<br>
-	<div class=ssTitle>Wave Red Storm Diffractor (#ed/10):</div> JM<br>
+	<div class=ssTitle>Wave Red Storm Diffractor (#ed/10):</div> JM</p>
 
 	<br>
 
-	<div class=title>2014 Topps Mini</div><br>
+	<p><div class=title>2014 Topps Mini</div><br>
 	<div class=ssTitle>Base:</div> 88, 103, 120, 125, 162, 220, 221, 325, 385, 405, 441, 463, 488, 492, 624<br>
 	<div class=ssTitle>Black (#/5)- HAVE:</div> 221<br>
 	<div class=ssTitle>Gold (#/63):</div> 12, 88, 103, 120, 125, 162, 220, 325, 385, 405, 441, 463, 488, 492, 518, 541, 568, 624<br>
-	<div class=ssTitle>Pink (#/25):</div> 12, 88, 103, 120, 125, 162, 220, 325, 385, 405, 441, 463, 488, 492, 518, 541, 568, 624<br>
+	<div class=ssTitle>Pink (#/25):</div> 12, 88, 103, 120, 125, 162, 220, 325, 385, 405, 441, 463, 488, 492, 518, 541, 568, 624</p>
 
 	<br>
 
-	<div class=title>2014 Topps Museum Collection</div><br>
+	<p><div class=title>2014 Topps Museum Collection</div><br>
 	<div class=ssTitle>Base:</div> 82<br>
 	<div class=ssTitle>Blue (#/99):</div> 82<br>
 	<div class=ssTitle>Copper:</div> 82<br>
 	<div class=ssTitle>Green (#/199):</div> 82<br>
-	<div class=ssTitle>Canvas Collection:</div> 5<br>
+	<div class=ssTitle>Canvas Collection:</div> 5</p>
 
 	<br>
 
-	<div class=title>2014 Topps Opening Day</div><br>
+	<p><div class=title>2014 Topps Opening Day</div><br>
 	<i>Completed- Breaking Out</i><br>
 	<div class=ssTitle>Base:</div> 73, 105<br>
 	<div class=ssTitle>Blue (#/2014):</div> 17, 73, 105, 175<br>
 	<div class=ssTitle>Stars:</div> 10<br>
-	<div class=ssTitle>Mascots:</div> 8<br>
+	<div class=ssTitle>Mascots:</div> 8</p>
 
 	<br>
 
-	<div class=title>2014 Topps Stickers</div><br>
-	<div class=ssTitle>Base:</div> 82, 83, 84, 85, 86, 87, 88, 89, 90, 144/158<br>
+	<p><div class=title>2014 Topps Stickers</div><br>
+	<div class=ssTitle>Base:</div> 82, 83, 84, 85, 86, 87, 88, 89, 90, 144/158</p>
 
 	<br>
 
-	<div class=title>2014 Topps Tribute</div><br>
+	<p><div class=title>2014 Topps Tribute</div><br>
 	<i>Completed- Base</i><br>
 	<div class=ssTitle>Blue (#/99):</div> 59, 65<br>
 	<div class=ssTitle>Green (#/50):</div> 59, 65<br>
-	<div class=ssTitle>Gold (#/25):</div> 59, 65<br>
+	<div class=ssTitle>Gold (#/25):</div> 59, 65</p>
 
 	<br>
 
-	<div class=title>2014 Topps Triple Threads</div><br>
+	<p><div class=title>2014 Topps Triple Threads</div><br>
 	<i>Completed- Base</i><br>
 	<div class=ssTitle>Amber (#/125):</div> 78<br>
 	<div class=ssTitle>Amethyst (#/325):</div> 78<br>
 	<div class=ssTitle>Emerald (#/250):</div> 78<br>
 	<div class=ssTitle>Gold (#/99):</div> 78<br>
 	<div class=ssTitle>Onyx (#/50):</div> 78<br>
-	<div class=ssTitle>Sapphire (#/25):</div> 78<br>
+	<div class=ssTitle>Sapphire (#/25):</div> 78</p>
 
 	<br>
 
-	<div class=title>2014 Topps Turkey Red</div><br>
-	<div class=ssTitle>Base:</div> 34<br>
+	<p><div class=title>2014 Topps Turkey Red</div><br>
+	<div class=ssTitle>Base:</div> 34</p>
 
 	<br>
 
-	<div class=title>2014 Topps Update</div><br>
+	<p><div class=title>2014 Topps Update</div><br>
 	<i>Completed- 1989 Minis, Fond Farewells</i><br>
 	<div class=ssTitle>Base:</div> 319b (Nolasco Saberstats SP)<br>
 	<div class=ssTitle>Black (#/63):</div> 125, 153, 182, 184, 224, 230, 237, 249, 257, 282, 319<br>
@@ -472,8 +470,9 @@
 	<div class=ssTitle>Pink (#/50):</div> 125, 153, 182, 184, 224, 230, 237, 249, 257, 282, 319<br>
 	<div class=ssTitle>Red Hot Foil:</div> 125, 153, 182, 224, 230, 237, 249, 282, 319<br>
 	<div class=ssTitle>Target Red:</div> 125, 153, 182, 184, 224, 230, 237, 249, 257, 282, 319<br>
-	<div class=ssTitle>Wal-Mart Blue:</div> 125, 182, 224, 230, 257, 282, 319<br>
+	<div class=ssTitle>Wal-Mart Blue:</div> 125, 182, 224, 230, 257, 282, 319</p>
 
+	<hr>
 
 </body>
-</html>
+</html
