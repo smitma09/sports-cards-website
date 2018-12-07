8a000641a2001801d4067aa1761c183f53be19fd
Hollywood42 <smitma09@luther.edu>
2018-12-07 01:23:20 +0000

    Updated develop galleries

--- develop/getCard.php
+++ develop/getCard.php
@@ -60,79 +60,32 @@
 		text-align: center;
 	}
 
-	/* The Modal (background) */
-	.modal {
-	    display: none; /* Hidden by default */
-	    position: fixed; /* Stay in place */
-	    z-index: 1; /* Sit on top */
-	    padding-top: 100px; /* Location of the box */
-	    left: 0;
-	    top: 0;
-	    width: 100%; /* Full width */
-	    height: 100%; /* Full height */
-	    overflow: auto; /* Enable scroll if needed */
-	    background-color: rgb(0,0,0); /* Fallback color */
-	    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
-	}
-
-	/* Modal Content (Image) */
-	.modal-content {
-	    margin: auto;
-	    display: block;
-	    width: 80%;
-	    max-width: 700px;
-	}
-
-	/* The Close Button */
-	.close {
-	    position: absolute;
-	    top: 15px;
-	    right: 35px;
-	    color: #f1f1f1;
-	    font-size: 40px;
-	    font-weight: bold;
-	    transition: 0.3s;
-	}
-
-	.close:hover,
-	.close:focus {
-	    color: #bbb;
-	    text-decoration: none;
-	    cursor: pointer;
-	}
-
 /* --------------------------------------------- */
 
 	</style>
 </head>
 <body>
-	<div id="theModal" class="modal">
-		<span class="close">&times;</span>
-		<span class="modal-content" id="modalImg"></span>
-		<div id="caption"></div>
-
-	</div>
-
-	<script>
-		// Get the modal
-		var modal = document.getElementById('theModal');
-
-		// Get the image and insert it inside the modal - use its "alt" text as a caption
-		var img = document.getElementById('modalImg');
-		var modalImg = document.getElementById("img01");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() { 
-		  modal.style.display = "none";
-		}
-	</script>
-
-
 <?php
 	include("/var/www/admin.php");
+	$year = $_GET['year'];
+	$playerFirst = $_GET['playerFirst'];
+	$playerLast = $_GET['playerLast'];
+	$cardSet = $_GET['cardSet'];
+	$subset = $_GET['subset'];
+	$search = $_GET['search'];
+	$auto = $_GET['auto'];
+	$relic = $_GET['relic'];
+	$patch = $_GET['patch'];
+	$manu = $_GET['manu'];
+	$rc = $_GET['rc'];
+	$sn = $_GET['sn'];
+	$oneofone = $_GET['oneofone'];
+	$hof = $_GET['hof'];
+	$spVar = $_GET['spvar'];
+	$graded = $_GET['graded'];
+	$grader = $_GET['grader'];
+
+//	echo "<h3>" . $auto . "</h3>";
 
 	$conn = mysqli_connect($dbServername, $publicdbUsername, $publicdbPass, $dbName);
 	if (!$conn) {
@@ -140,8 +93,141 @@
 	    echo '$conn';
 	}
 
-	$sql = 'select * from twins_pc where year=2013 and playerLast="Morneau" order by year desc, cardset asc, subset asc, cast(cardNum as signed) asc';
+	$sql = 'select * from twins_pc';
 	
+	/* ---------- Creating SQL statement ---------- */
+	$clauses = 0;
+	if ($year != "All") {
+		if ($clauses == 0) {
+			$sql = $sql . ' where year = "' . $year . '" and';
+			$clauses = $clauses + 1;
+		} else {
+		$sql = $sql . ' year = "' . $year . '" and';
+		}
+	} if ($cardSet != "All") {
+		if ($clauses == 0) {
+			$sql = $sql . ' where cardSet = "' . $cardSet . '" and';
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . ' cardSet = "' . $cardSet . '" and';
+		}
+	} if ($subset != "All") {
+		if ($clauses == 0) {
+			$sql = $sql . ' where subset = "' . $subset . '" and';
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . ' subset = "' . $subset . '" and';
+		}
+	} if ($playerFirst != "All") {
+		if ($clauses == 0) {
+			$playerFirst = str_replace("'", "''", $playerFirst);
+			$sql = $sql . " where (playerFirst = '" . $playerFirst . "' or allPlayers like '%" . $playerFirst . " %') and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " (playerFirst = '" . $playerFirst . "' or allPlayers like '%" . $playerFirst . " %') and";
+		}
+	}
+	if ($playerLast != "All") {
+		if ($clauses == 0) {
+			$playerLast = str_replace("'", "''", $playerLast);
+			$sql = $sql . " where (playerLast = '" . $playerLast . "' or allPlayers like '% " . $playerLast . "%') and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " (playerLast = '" . $playerLast . "' or allPlayers like '% " . $playerLast . "%') and";
+		}
+	} if ($auto == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where auto = '" . $auto . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " auto = '" . $auto . "' and";
+		}
+	} if ($relic == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where relic = '" . $relic . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " relic = '" . $relic . "' and";
+		}
+	} if ($patch == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where patch = '" . $patch . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " patch = '" . $patch . "' and";
+		}
+	} if ($manu == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where manuRelic = '" . $manu . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " manuRelic = '" . $manu . "' and";
+		}
+	} if ($rc == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where rc = '" . $rc . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " rc = '" . $rc . "' and";
+		}
+	} if ($sn == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where numbered = '" . $sn . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " numbered = '" . $sn . "' and";
+		}
+	} if ($oneofone == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where oneofone = '" . $oneofone . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " oneofone = '" . $oneofone . "' and";
+		}
+	} if ($hof == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where hof = '" . $hof . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " hof = '" . $hof . "' and";
+		}
+	} if ($spVar == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where spVar = '" . $spVar . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " spVar = '" . $spVar . "' and";
+		}
+	} if ($graded == "1") {
+		if ($clauses == 0) {
+			$sql = $sql . " where graded = '" . $graded . "' and";
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . " graded = '" . $graded . "' and";
+		}
+	} if ($grader != "All") {
+		if ($clauses == 0) {
+			$sql = $sql . ' where grader = "' . $grader . '" and';
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . ' grader = "' . $grader . '" and';
+		}
+	} if ($search != "") {
+		if ($clauses == 0) {
+			$sql = $sql . ' where fullCardInfo like "%' . $search . '%" and';
+			$clauses = $clauses + 1;
+		} else {
+			$sql = $sql . ' fullCardInfo like "%' . $search . '%" and';
+		}
+	}
+
+	// Need to chop of the last ' and' from the sql statement
+	$sql = substr($sql, 0, -4);
+	$sql = $sql . ' order by year desc, cardset asc, subset asc, cast(cardNum as signed) asc';
+
+	if ($playerFirst) {
+		echo $playerLast;
+	}
 
 	$result = mysqli_query($conn, $sql);
 	$num_cards = mysqli_num_rows($result);
@@ -156,12 +242,11 @@
 		$wwwImg = substr($pic, 13);
 			//echo "<span id=aCard><img src=" . $wwwImg . " height ='250px'>";
 			//echo "<span class=text>" . $row['fullCardInfo'] . "</span></span>";
-			echo "<div class=fullContainer><div class=imgContainer><img class=image id=" .$row['fullCardInfo'] . "src=" . $wwwImg ."></div><p class=text>" . $row['fullCardInfo'] . "</p></div>";
+			echo "<div class=fullContainer><div class=imgContainer><img class=image src=" . $wwwImg ."></div><p class=text>" . $row['fullCardInfo'] . "</p></div>";
 		}
 		echo "</div></div>";
 	}
 	mysqli_close($conn);
 ?>
-
 </body>
 </html>
